3111c2f473fb8ac9ee20bf8580825a01ba43efd1
Sean Myers <spmyers@protonmail.com>
2021-06-18 07:42:02 -0400

    fixing update chronicle query - removing column specification for update clause - removing quotations around id value in query string - selecting correct input in string for id by incrementing by 1 - removing string conversion of id in code

--- backend/src/data/query/chronicle/update_chronicle.rs
+++ backend/src/data/query/chronicle/update_chronicle.rs
@@ -9,18 +9,18 @@ use crate::data::entity::chronicle::Chronicle;
 
 const UPDATE_CHRONICLE_QUERY : &str = "
     UPDATE
-        public.chronicle (date_recorded)
+        public.chronicle
     SET 
       date_recorded = (TO_DATE($1, 'YYYY-MM-DD'))
     WHERE
-      id = '$1'
+      id = $2
    RETURNING
       id
       , date_recorded
     ;";
 
 pub async fn update_chronicle_query(database_connection: &Connection<PgConnectionManager<NoTls>>, chronicle_to_update: &Chronicle) -> Result<Chronicle> {
-    let query_result = database_connection.query_one(UPDATE_CHRONICLE_QUERY, &[&chronicle_to_update.date_recorded.to_string(), &chronicle_to_update.id.to_string()])
+    let query_result = database_connection.query_one(UPDATE_CHRONICLE_QUERY, &[&chronicle_to_update.date_recorded.to_string(), &chronicle_to_update.id])
     .await.context(format!("An error occurred while querying the database."))?;
     let result_id: Uuid = query_result.get::<_, Uuid>("id");
     let result_date: Date<Utc> = Utc.from_utc_date(&query_result.get::<_, NaiveDate>("date_recorded"));
