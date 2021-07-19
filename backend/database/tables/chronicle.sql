b66f9be9174646112cc9401b932d5afaa9edf24d
Sean Myers <spmyers@protonmail.com>
2021-07-19 09:44:23 -0400

    updaing chronicle sql table generation script - id is not special column name

--- backend/database/tables/chronicle.sql
+++ backend/database/tables/chronicle.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "public".chronicle
 (
  date_recorded date NOT NULL,
- "id"            uuid NOT NULL,
+ id            uuid NOT NULL,
  CONSTRAINT PK_chronicle PRIMARY KEY ( date_recorded, "id" )
 );
