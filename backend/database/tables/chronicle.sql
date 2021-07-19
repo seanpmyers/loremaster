433e77be36cbebbde4d0970b47a8f8500078aedf
Sean Myers <spmyers@protonmail.com>
2021-07-19 12:41:02 -0400

    adding default generate uuid to primary key for chronicle

--- backend/database/tables/chronicle.sql
+++ backend/database/tables/chronicle.sql
@@ -1,6 +1,6 @@
-CREATE TABLE "public".chronicle
-(
- date_recorded date NOT NULL,
- id            uuid NOT NULL,
- CONSTRAINT PK_chronicle PRIMARY KEY ( date_recorded, "id" )
-);
+CREATE TABLE public.chronicle (
+      date_recorded date NOT NULL,
+      id uuid NOT NULL DEFAULT uuid_generate_v4(),
+      CONSTRAINT pk_chronicle PRIMARY KEY (date_recorded, id)
+   )
+;
