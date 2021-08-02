96949b9440502f4ecb0a1f93a534da5dd73aaa28
Sean Myers <spmyers@protonmail.com>
2021-08-02 09:13:23 -0400

    adding a person entity for new users

--- backend/src/data/entity/person.rs
+++ backend/src/data/entity/person.rs
@@ -1,8 +1,12 @@
-
-
 #[derive(Debug)]
 pub struct Person {
     pub id : Uuid,
     pub email_address : String,
     pub alias : Option<String>,
+}
+
+pub struct PersonCredentials {
+    pub id: Uuid,
+    pub email_address: String,
+    pub password: String
 }
