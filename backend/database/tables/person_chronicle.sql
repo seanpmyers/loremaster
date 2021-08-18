ed224caacd1c0aacc9f56ceb6c943e68c88c2636
Sean Myers <spmyers@protonmail.com>
2021-08-18 06:56:39 -0400

    updating person chronicle table sql script

--- backend/database/tables/person_chronicle.sql
+++ backend/database/tables/person_chronicle.sql
@@ -1,20 +1,12 @@
-CREATE TABLE person_chronicle
-(
- chronicle_id  uuid NOT NULL,
- date_recorded date NOT NULL,
- person_id     uuid NOT NULL,
- CONSTRAINT PK_person_chronicle PRIMARY KEY ( date_recorded, chronicle_id, person_id ),
- CONSTRAINT FK_198 FOREIGN KEY ( person_id ) REFERENCES "public".person ( "id" ),
- CONSTRAINT FK_205 FOREIGN KEY ( date_recorded, chronicle_id ) REFERENCES "public".chronicle ( date_recorded, "id" )
+CREATE TABLE public.person_chronicle (
+	person_id uuid NOT NULL,
+	chronicle_id uuid NOT NULL,
+	date_recorded date NOT NULL,
+	CONSTRAINT person_chronicle_pk PRIMARY KEY (person_id, date_recorded, chronicle_id)
 );
 
-CREATE INDEX fkIdx_199 ON person_chronicle
-(
- person_id
-);
 
-CREATE INDEX fkIdx_206 ON person_chronicle
-(
- chronicle_id,
- date_recorded
-);
+-- public.person_chronicle foreign keys
+
+ALTER TABLE public.person_chronicle ADD CONSTRAINT person_chronicle_fk FOREIGN KEY (person_id) REFERENCES public.person(id);
+ALTER TABLE public.person_chronicle ADD CONSTRAINT person_chronicle_fk_1 FOREIGN KEY (date_recorded,chronicle_id) REFERENCES public.chronicle(date_recorded,id);
