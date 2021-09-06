01396f2e9a11843bd5eea3377ca518e1b0225aa9
Sean Myers <spmyers@protonmail.com>
2021-09-06 08:20:19 -0400

    fixing delete chronicle query

--- backend/src/data/query/chronicle/delete_chronicle.rs
+++ backend/src/data/query/chronicle/delete_chronicle.rs
@@ -10,11 +10,11 @@ const DELETE_CHRONICLE_QUERY : &str = "
     DELETE FROM
         public.chronicle
     WHERE
-      chronicle.id = '$1'
+      chronicle.id = $1
     ;";
 
 pub async fn delete_chronicle_query(database_connection: &Connection<PgConnectionManager<NoTls>>, chronicle_id: &Uuid) -> Result<()> {
-    database_connection.query(DELETE_CHRONICLE_QUERY, &[&chronicle_id.to_string()]).await.context(format!("An error occurred while deleting chronicle with id: {}.", chronicle_id))?;
+    database_connection.query(DELETE_CHRONICLE_QUERY, &[&chronicle_id]).await.context(format!("An error occurred while deleting chronicle with id: {}.", chronicle_id))?;
     return Ok(());
 }
 
