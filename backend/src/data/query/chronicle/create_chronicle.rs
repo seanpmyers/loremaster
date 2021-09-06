cd74b013683ba5e3c3a5ffdc2830440c4fff555f
Sean Myers <spmyers@protonmail.com>
2021-09-06 08:20:02 -0400

    adding optional id field for chronicle creation

--- backend/src/data/query/chronicle/create_chronicle.rs
+++ backend/src/data/query/chronicle/create_chronicle.rs
@@ -16,14 +16,45 @@ const CREATE_CHRONICLE_QUERY : &str = "
         id
     ;";
 
-pub async fn create_chronicle_query(database_connection: &Connection<PgConnectionManager<NoTls>>, chronicle_date: &Date<Local>) -> Result<Chronicle> {
-    let query_result = database_connection.query_one(CREATE_CHRONICLE_QUERY, &[&chronicle_date.to_string()]).await.context(format!("An error occurred while querying the database."))?;
-    let result_id: Uuid = query_result.get::<_, Uuid>("id");
+const CREATE_CHRONICLE_QUERY_WITH_ID : &str = "
+    INSERT INTO
+        public.chronicle (id, date_recorded)
+    VALUES 
+    ($1, TO_DATE($2, 'YYYY-MM-DD'))
+    RETURNING
+        id
+    ;";
 
-    let new_chronicle: Chronicle = Chronicle{
-        id: result_id,
-        date_recorded: chronicle_date.to_owned()
-    };
+pub async fn create_chronicle_query(database_connection: &Connection<PgConnectionManager<NoTls>>, chronicle_date: &Date<Local>, chronicle_id:  &Option<Uuid>) -> Result<Chronicle> {
+    match chronicle_id {
+        Some(id) => {
+            let query_result = database_connection
+            .query_one(CREATE_CHRONICLE_QUERY_WITH_ID, &[&id, &chronicle_date.to_string()])
+            .await.context(format!("An error occurred while querying the database."))?;
 
-    return Ok(new_chronicle);
+            let result_id: Uuid = query_result.get::<_, Uuid>("id");
+
+            let new_chronicle: Chronicle = Chronicle{
+                id: result_id,
+                date_recorded: chronicle_date.to_owned()
+            };
+
+            return Ok(new_chronicle);
+        },
+        None => {
+            let query_result = database_connection
+            .query_one(CREATE_CHRONICLE_QUERY, &[&chronicle_date.to_string()])
+            .await.context(format!("An error occurred while querying the database."))?;
+    
+            let result_id: Uuid = query_result.get::<_, Uuid>("id");
+        
+            let new_chronicle: Chronicle = Chronicle{
+                id: result_id,
+                date_recorded: chronicle_date.to_owned()
+            };
+        
+            return Ok(new_chronicle);
+        }
+    }
+
 }
