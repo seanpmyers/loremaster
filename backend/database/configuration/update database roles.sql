c9a364c1c103de4d6fa953ff88e41e7c34996f9a
Sean Myers <spmyers@protonmail.com>
2021-09-06 07:42:43 -0400

    updating databse broker role name adding some database role setup privledges

--- backend/database/configuration/update database roles.sql
+++ backend/database/configuration/update database roles.sql
@@ -1,8 +1,51 @@
 ALTER ROLE developer CREATEDB CREATEROLE NOSUPERUSER;
 
 GRANT developer TO naes;
-GRANT broker TO loremaster_broker;
+GRANT application_broker TO loremaster_broker;
 
 --ALTER ROLE '' WITH PASSWORD NULL;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA  public TO loremaster_broker;
+GRANT 
+	SELECT
+	, UPDATE
+	, INSERT
+	, DELETE
+	, TRUNCATE
+	, REFERENCES
+	, CREATE
+	, CONNECT
+	, EXECUTE
+	, USAGE
+	, TRIGGER
+	, TEMPORARY TEMP
+ON 
+	ALL TABLES
+IN SCHEMA 
+	"public"
+TO 
+	developer
+;
+
+GRANT 
+	ALL PRIVILEGES 
+ON 
+	ALL TABLES
+IN SCHEMA
+	"public"
+TO
+	developer
+;
+
+
+GRANT 
+   SELECT
+   , INSERT
+   , UPDATE
+   , DELETE
+ON 
+   ALL TABLES
+IN SCHEMA 
+   "public"
+TO 
+	application_broker
+;
