ecb293dc5c8d6928ce1376eadb765d520e812dc1
Sean Myers <spmyers@protonmail.com>
2021-12-14 07:51:19 -0500

    updating intention sql schema

--- backend/database/tables/intention.sql
+++ backend/database/tables/intention.sql
@@ -1,6 +1,6 @@
-CREATE TABLE "public".intention
-(
- "id"   uuid NOT NULL,
- Name text NOT NULL,
- CONSTRAINT PK_goal PRIMARY KEY ( "id" )
+CREATE TABLE "public".intention (
+   id uuid NOT NULL DEFAULT uuid_generate_v4(),
+   name text NOT NULL,
+   CONSTRAINT PK_goal PRIMARY KEY (id),
+   CONSTRAINT name_unique UNIQUE (name)
 );
