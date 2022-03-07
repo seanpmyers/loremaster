49669e603fa0a52da953ceba965e9fcc2acc9cd4
Sean Myers <spmyers@protonmail.com>
2022-03-07 07:56:40 -0500

    removing redundant error information

--- application/src/utility/toml_reader.rs
+++ application/src/utility/toml_reader.rs
@@ -1,15 +1,15 @@
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, Result};
 use std::fs;
 use toml::Value;
 
-pub fn get_field_value(file_name: &str, field_name: &str) -> Result<String> {
-   let string_file_content : String = fs::read_to_string(file_name)
-      .context("Something went wrong reading the file".to_string())?;
-   let value_map: Value =  string_file_content.parse::<Value>()
-      .context("Something went wrong while parsing toml file's string content!".to_string())?;
-   if let Some(value) = value_map[field_name].as_str() {
-      return Ok(value.to_string())
-   } else {
-      return Err(anyhow!("No value found for field name: {}", field_name))
-   };
+pub fn get_field_value(file_path: &str, field_name: &str) -> Result<String> {
+    let string_file_content: String = fs::read_to_string(file_path)?;
+
+    let value_map: Value = string_file_content.parse::<Value>()?;
+
+    if let Some(value) = value_map[field_name].as_str() {
+        return Ok(value.to_string());
+    } else {
+        return Err(anyhow!("No value found for field name: {}", field_name));
+    };
 }
