48dd2a249ed997830bc3661d902c3334887c20c9
Sean Myers <spmyers@protonmail.com>
2022-05-11 08:33:36 -0400

    Updating dependencies

--- application/frontend/src/components/registration.rs
+++ application/frontend/src/components/registration.rs
@@ -1,6 +1,6 @@
 use reqwasm::http::Request;
 use serde::{Deserialize, Serialize};
-use sycamore::{prelude::*, suspense::Suspense};
+use sycamore::{futures::spawn_local_scoped, prelude::*, suspense::Suspense};
 
 use crate::utility::constants::API_REGISTER_URL;
 
@@ -9,67 +9,69 @@ struct RegisterResponse {
     value: String,
 }
 
-// async fn register_user(
-//     email_address: String,
-//     password: String,
-// ) -> Result<RegisterResponse, reqwasm::Error> {
-//     let response: reqwasm::http::Response = Request::post(&format!(
-//         "{API_REGISTER_URL}/?email_address={email_address}&password={password}"
-//     ))
-//     .send()
-//     .await?;
+async fn register_user(
+    email_address: String,
+    password: String,
+) -> Result<RegisterResponse, reqwasm::Error> {
+    let response: reqwasm::http::Response = Request::post(&format!(
+        "{API_REGISTER_URL}/?email_address={email_address}&password={password}"
+    ))
+    .send()
+    .await?;
 
-//     let body: RegisterResponse = response.json::<RegisterResponse>().await?;
+    let body: RegisterResponse = response.json::<RegisterResponse>().await?;
 
-//     Ok(body)
-// }
+    Ok(body)
+}
 
-// #[component]
-// pub async fn RegistrationForm<G: Html>(context: Scope<'_>) -> View<G> {
-//     let email_address: &Signal<String> = context.create_signal(String::new());
-//     let password: &Signal<String> = context.create_signal(String::new());
+#[component]
+pub async fn RegistrationForm<G: Html>(context: Scope<'_>) -> View<G> {
+    let email_address: &Signal<String> = create_signal(context, String::new());
+    let password: &Signal<String> = create_signal(context, String::new());
 
-//     let submit_registration = |_| {
-//         if email_address.get().is_empty() || password.get().is_empty() {
-//             return;
-//         }
-//         let response = register_user(
-//             email_address.get().as_ref().clone(),
-//             password.get().as_ref().clone(),
-//         )
-//         .await
-//         .unwrap_or_default();
-//     };
+    let submit_registration = move |_| {
+        spawn_local_scoped(context, async move {
+            if email_address.get().is_empty() || password.get().is_empty() {
+                return;
+            }
+            let response = register_user(
+                email_address.get().as_ref().clone(),
+                password.get().as_ref().clone(),
+            )
+            .await
+            .unwrap_or_default();
+        })
+    };
 
-//     view! { context,
-//             form {
-//                 div {
-//                     label {"Email Address"}
-//                     input(placeholder="you@loremaster.xyz", type="text", bind=email_address.clone())
-//                 }
-//                 div {
-//                     label {"Password"}
-//                     input(placeholder="", type="password", bind=password.clone())
-//                 }
-//                 div {
-//                     button(on:submit=submit_registration)
-//                 }
-//         }
-//     }
-// }
+    view! { context,
+            form {
+                div {
+                    label {"Email Address"}
+                    input(placeholder="you@loremaster.xyz", type="text", bind=email_address.clone())
+                }
+                div {
+                    label {"Password"}
+                    input(placeholder="", type="password", bind=password.clone())
+                }
+                div {
+                    button(type="submit", on:submit=submit_registration)
+                }
+        }
+    }
+}
 
-// #[component]
-// pub fn Registration<G: Html>(context: Scope) -> View<G> {
-//     view! { context,
-//         div {
-//             h1 {
-//                 "Register"
-//             }
-//             Suspense {
-//                 fallback: view!{ context, "Loading..."},
-//                 RegistrationForm {}
-//             }
+#[component]
+pub fn Registration<G: Html>(context: Scope) -> View<G> {
+    view! { context,
+        div {
+            h1 {
+                "Register"
+            }
+            Suspense {
+                fallback: view!{ context, "Loading..."},
+                RegistrationForm {}
+            }
 
-//         }
-//     }
-// }
+        }
+    }
+}
