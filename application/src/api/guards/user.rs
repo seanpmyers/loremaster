ed94082e102a8be658371a9a0e608a35f2e6e9b3
Sean Myers <spmyers@protonmail.com>
2022-08-04 08:21:15 -0400

    Updating user guard to show the correct error message

--- application/src/api/guards/user.rs
+++ application/src/api/guards/user.rs
@@ -26,24 +26,12 @@ where
                     let user_id = Uuid::parse_str(cookie.value()).unwrap();
                     Ok(User(user_id))
                 }
-                None => Err((
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    "Failed to match authorization.",
-                )),
+                None => Err((StatusCode::UNAUTHORIZED, "No authorization found!")),
             },
-            Err(_) => Err((StatusCode::UNAUTHORIZED, "No authorization found!")),
+            Err(_) => Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Failed to evalute authorization.",
+            )),
         }
     }
-
-    // type Error = std::convert::Infallible;
-
-    // async fn from_request(request: &'r Request<'_>) -> request::Outcome<User, Self::Error> {
-    //     info!("Checking for user_id cookie.");
-    //     let user_id: Option<Uuid> = request
-    //         .cookies()
-    //         .get_private(USER_ID)
-    //         .and_then(|cookie| Uuid::parse_str(cookie.value()).ok());
-
-    //     return user_id.map(User).or_forward(());
-    // }
 }
