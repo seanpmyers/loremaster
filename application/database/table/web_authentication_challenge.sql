394eca101971f8ffe1e642b96ad6738501a99a7f
Sean Myers <spmyers@protonmail.com>
2023-05-03 08:53:36 -0400

    updating dependencies

--- application/database/table/web_authentication_challenge.sql
+++ application/database/table/web_authentication_challenge.sql
@@ -2,5 +2,5 @@ CREATE TABLE
     "web_authentication_challenge" (
         "id" uuid NOT NULL PRIMARY KEY,
         "user_name" text NOT NULL,
-        "passkey_registration" json NOT NULL
+        "passkey_registration" jsonb NOT NULL
     );
--- application/database/table/web_authentication_key.sql
+++ application/database/table/web_authentication_key.sql
@@ -3,5 +3,5 @@ CREATE TABLE
         "id" uuid NOT NULL PRIMARY KEY,
         "credential_id" bytea NOT NULL,
         "cose_algorithm" int NOT NULL,
-        "passkey" json NOT NULL
+        "passkey" jsonb NOT NULL
     );
